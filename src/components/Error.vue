3c86b2e3c711d4696a79c9a8366ddc7e454603bf
Phi <phingoc88@hotmail.com>
2023-05-07 14:15:43 +0200

    made some uu improvments

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,6 +1,6 @@
 <template>
     <div role="alert" class="osg-status-message osg-status-message--danger osg-margin-top-4">
-        <p class="osg-status-message__heading"><span class="osg-status-message__icon osg-icon--success-square" aria-hidden="true"></span>{{error}}</p>
+        <p class="osg-status-message__heading"><span class="osg-status-message__icon osg-icon--error-hexa" aria-hidden="true"></span>{{error}}</p>
     </div>
 </template>
 
